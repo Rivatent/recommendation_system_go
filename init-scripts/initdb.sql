2b98d1bb425eec0af53c8755d042190002cf59ff
Riva Tentacle <rivatent@student.21-school.ru>
2024-12-18 13:10:57 +0200

    feat: add timestamp for users table

--- init-scripts/initdb.sql
+++ init-scripts/initdb.sql
@@ -1,12 +1,26 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     name TEXT NOT NULL,
-                                     email TEXT UNIQUE NOT NULL
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       username VARCHAR(50) UNIQUE NOT NULL,
+                       email VARCHAR(100) UNIQUE NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (name, email) VALUES
-                                    ('Иван Иванов', 'ivan.ivanov@example.com'),
-                                    ('Мария Петрова', 'maria.petrova@example.com'),
-                                    ('Алексей Смирнов', 'alexey.smirnov@example.com'),
-                                    ('Ольга Кузнецова', 'olga.kuznetsova@example.com'),
-                                    ('Дмитрий Васильев', 'dmitry.vasiliev@example.com');
+INSERT INTO users (username, email) VALUES
+                                        ('Иван Иванов', 'ivan.ivanov@example.com'),
+                                        ('Мария Петрова', 'maria.petrova@example.com'),
+                                        ('Алексей Смирнов', 'alexey.smirnov@example.com'),
+                                        ('Ольга Кузнецова', 'olga.kuznetsova@example.com'),
+                                        ('Дмитрий Васильев', 'dmitry.vasiliev@example.com'),
+                                        ('Анна Сергеева', 'anna.sergeeva@example.com'),
+                                        ('Сергей Орлов', 'sergey.orlov@example.com'),
+                                        ('Елена Фёдорова', 'elena.fyodorova@example.com'),
+                                        ('Виктория Лебедева', 'victoria.lebedyeva@example.com'),
+                                        ('Артем Громов', 'artem.gromov@example.com'),
+                                        ('Наталья Сидорова', 'natalya.sidorova@example.com'),
+                                        ('Роман Николаев', 'roman.nikolaev@example.com'),
+                                        ('Татьяна Михайлова', 'tatiana.mikhaylova@example.com'),
+                                        ('Станислав Крылов', 'stanislav.krylov@example.com'),
+                                        ('Ксения Алексеева', 'kseniya.alekseeva@example.com'),
+                                        ('Павел Гречишкин', 'pavel.grecheshkin@example.com'),
+                                        ('Ирина Дмитриева', 'irina.dmitrieva@example.com');
--- user-service/internal/repository/user.go
+++ user-service/internal/repository/user.go
@@ -1,15 +1,20 @@
 package repository
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (r *Repo) GetUsersRepo() ([]User, error) {
-	rows, err := r.db.Query("SELECT id, name, email FROM users")
+	rows, err := r.db.Query("SELECT id, username, email, created_at, updated_at FROM users")
 	if err != nil {
 		return nil, err
 	}
@@ -18,7 +23,7 @@ func (r *Repo) GetUsersRepo() ([]User, error) {
 	var users []User
 	for rows.Next() {
 		var user User
-		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+		if err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.CreatedAt, &user.UpdatedAt); err != nil {
 			return nil, err
 		}
 		users = append(users, user)
@@ -28,34 +33,37 @@ func (r *Repo) GetUsersRepo() ([]User, error) {
 }
 
 func (r *Repo) CreateUserRepo(user User) (User, error) {
-	if user.Name == "" || user.Email == "" {
-		return User{}, errors.New("name and email are required")
+	if user.Username == "" || user.Email == "" {
+		return User{}, errors.New("username and email are required")
 	}
 
-	query := `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id`
+	query := `INSERT INTO users (username, email) VALUES ($1, $2) RETURNING id, created_at, updated_at`
 	var userID int
-	err := r.db.QueryRow(query, user.Name, user.Email).Scan(&userID)
+	var createdAt, updatedAt time.Time
+	err := r.db.QueryRow(query, user.Username, user.Email).Scan(&userID, &createdAt, &updatedAt)
 	if err != nil {
 		return User{}, err
 	}
 	user.ID = userID
+	user.CreatedAt = createdAt
+	user.UpdatedAt = updatedAt
 
 	return user, nil
 }
 
 func (r *Repo) UpdateUserRepo(user User) (User, error) {
-	query := `UPDATE users SET name = $1, email = $2 WHERE id = $3`
-	_, err := r.db.Exec(query, user.Name, user.Email, user.ID)
+	query := `UPDATE users SET username = $1, email = $2, updated_at = CURRENT_TIMESTAMP WHERE id = $3`
+	_, err := r.db.Exec(query, user.Username, user.Email, user.ID)
 	if err != nil {
 		return User{}, err
 	}
-	return user, err
+	return user, nil
 }
 
 func (r *Repo) GetUserByIDRepo(id int) (User, error) {
 	var user User
-	row := r.db.QueryRow("SELECT id, name, email FROM users WHERE id = $1", id)
-	err := row.Scan(&user.ID, &user.Name, &user.Email)
+	row := r.db.QueryRow("SELECT id, username, email, created_at, updated_at FROM users WHERE id = $1", id)
+	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		return User{}, err
 	}
