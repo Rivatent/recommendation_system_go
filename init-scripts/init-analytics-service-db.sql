54b7a7176c43fd9e66c2506abaf26e97df2e527b
Riva Tentacle <rivatent@student.21-school.ru>
2024-12-23 14:56:34 +0200

    feat: add table for analytics

---
+++ init-scripts/init-analytics-service-db.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE analytics (
+                          id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                          total_users INT DEFAULT 0,
+                          total_sales INT DEFAULT 0,
+                          sales_progression_rate DECIMAL(3,2) DEFAULT 0.00,
+                          users_progression_rate DECIMAL(3,2) DEFAULT 0.00,
+                          product_average_rating DECIMAL(3,2) DEFAULT 0.00,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO analytics (total_users, total_sales, sales_progression_rate, users_progression_rate, product_average_rating)
+VALUES
+    (5, 3, 0.60, 0.50, 4.00),
+    (10, 7, 0.70, 1.00, 4.00),
+    (17, 13, 0.76, 1.70, 4.00);
